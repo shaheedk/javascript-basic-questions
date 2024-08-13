ebc4249a1ec05e2979e171f9e8f0b28d11500ded
Shaheed mon <sheheedk07@gmail.com>
2024-08-13 21:11:33 +0530

    commit

---
+++ singleNumber.js
@@ -0,0 +1,15 @@
+// Given a non-empty array of integers, every element appears twice except for one.
+// Find that single one. 
+// Examples
+// singleNumber([2, 2, 1]) ➞ 1
+// singleNumber([4, 1, 2, 1, 2]) ➞ 4
+// singleNumber([3]) ➞ 3
+function singleNumber(nums) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
+console.log(singleNumber([2, 2, 1]))
+console.log(singleNumber([4, 1, 2, 1, 2]))
