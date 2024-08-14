bff3db5f1c484d25693022c8d0763920b1774040
Shaheed mon <sheheedk07@gmail.com>
2024-08-14 11:12:01 +0530

    commit

---
+++ hoursToSeconds.js
@@ -0,0 +1,9 @@
+// Write a function that converts hours into seconds.
+// Examples
+// hoursToSeconds(10) ➞ 36000
+// hoursToSeconds(24) ➞ 86400
+function hoursToSeconds(hours){
+    return hours*3600
+}
+console.log(hoursToSeconds(10))
+console.log(hoursToSeconds(24))
