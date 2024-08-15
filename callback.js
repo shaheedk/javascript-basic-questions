1fc308cee54bdcb67c514cd78e53b1d6c18868a0
Shaheed mon <sheheedk07@gmail.com>
2024-08-15 18:00:46 +0530

    commit

---
+++ callback.js
@@ -0,0 +1,33 @@
+console.log('start')
+function getDataFromApi1(callback){
+setTimeout(()=>{
+    callback(5)
+},1000)
+}
+function getDataFromApi3(callback){
+    setTimeout(()=>{
+        callback(5)
+    },1000)
+    }
+    function getDataFromApi2(callback){
+        setTimeout(()=>{
+            callback(5)
+        },1000)
+        }
+        getDataFromApi1((value)=>{
+            getDataFromApi2((api2val)=>{
+                
+           
+            getDataFromApi3((api3val)=>{
+                
+            console.log(api2val+api3val+value)
+    
+        })
+    })
+})
+        
+
+
+
+
+        console.log('end')
