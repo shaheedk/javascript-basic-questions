1d917df2f5e5b9ec72225b6615e7d4a9a4aea54f
Shaheed mon <sheheedk07@gmail.com>
2024-08-16 21:44:50 +0530

    commit

---
+++ findLargetNumber.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function called findLargestNumber
+//  that takes an array of numbers and returns the largest number in the array.
+
+
+ function findLargestNumber(arr){
+return Math.max(...arr)
+ }
+ console.log(findLargestNumber([10, 5, 8, 22, 17]))
+
+ 
