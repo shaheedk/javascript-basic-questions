3cb18a2c3fe6d72cbb75c8867876c2ffe95aea06
Shaheed mon <sheheedk07@gmail.com>
2024-08-17 17:03:20 +0530

    commit

---
+++ Remainder.js
@@ -0,0 +1,25 @@
+// Return the Remainder from Two Numbers
+// There is a single operator in JavaScript, capable of providing the
+//  remainder of a division operation. Two numbers are passed as
+//   parameters. The first parameter divided by the second parameter 
+//   will have a remainder, possibly zero. Return that value.
+
+// Examples
+// remainder(1, 3) ➞ 1
+
+// remainder(3, 4) ➞ 3
+
+// remainder(-9, 45) ➞ -9
+
+// remainder(5, 5) ➞ 0
+
+function remainder(x,y){
+    if (x==y){
+        return 0
+    }else{
+        return x
+    }
+
+}
+console.log( remainder(5, 5))
+console.log(remainder(-9, 45))
