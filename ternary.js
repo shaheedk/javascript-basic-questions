06396d61e74675dda26de2124a40ac2e574e47d9
Shaheed mon <sheheedk07@gmail.com>
2024-08-30 22:19:43 +0530

    commit

---
+++ ternary.js
@@ -0,0 +1,10 @@
+// positive or negative
+function check(number){
+    
+let result = (number === 0) ? "Zero" : (number > 0) ? "Number is nositive" : " Number is negative";
+
+console.log(result);
+}
+check(-2)
+check(3)
+check(0)
