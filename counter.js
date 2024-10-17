72fa39864b70ffb0594d9efc24b81b3e7b829cae
Shaheed mon <sheheedk07@gmail.com>
2024-10-17 16:13:19 +0530

    commit

---
+++ counter.js
@@ -0,0 +1,13 @@
+var createCounter = function(n) {
+    let count=1
+    
+        return function() {
+            if(count===1){
+                count++
+                return n
+            }else{
+                return   n=n+1
+            }
+    
+        };
+    };
