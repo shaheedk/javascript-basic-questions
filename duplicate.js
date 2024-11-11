7a04a6381ea1414e3b70ae4a14982ec757eab25b
Shaheed mon <sheheedk07@gmail.com>
2024-11-11 11:21:10 +0530

    commit

--- duplicate.js
+++ duplicate.js
@@ -1,10 +1,10 @@
-// const list = [1, 2, 3, 2, 3, 10, 5, 3, 1, 2, "php", "javascript", "python", "php", "java", "php", "ruby", "python"];
-const list=[1,2,2,3,3,4]
-// console.log(list);
+const list = [1, 2, 3, 2, 3, 10, 5, 3, 1, 2, "php", "javascript", "python", "php", "java", "php", "ruby", "python"];
+// const list2=[1,2,2,3,3,4]
+console.log(list);
 
 // using filter function
 function removeDuplicates(array) {
   return array.filter((el, index) => array.indexOf(el) === index);
 }
 
-console.log(removeDuplicates(list));
+console.log(removeDuplicates(list));    
